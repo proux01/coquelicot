b9d6624d091ca25498066dda22307bdb4d00bb28
Laurent Théry <laurent.thery@inria.fr>
2020-12-11 11:29:06 +0100

    Adapt to the weaker hypothesis of exp_ineq1.

--- theories/ElemFct.v
+++ theories/ElemFct.v
@@ -601,7 +601,7 @@ Lemma is_lim_exp_p : is_lim (fun y => exp y) p_infty p_infty.
 Proof.
   apply is_lim_le_p_loc with (fun y => 1 + y).
   exists 0 => y Hy.
-  by apply Rlt_le, exp_ineq1.
+  by apply Rlt_le, exp_ineq1; auto with real.
   pattern p_infty at 2.
   replace p_infty with (Rbar_plus 1 p_infty) by auto.
   eapply is_lim_plus.
